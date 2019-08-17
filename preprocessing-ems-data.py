da193e8ac3afbccb2f3d32c981117ccb438ba1d7
Shreyansh Anand <s.anand@queensu.ca>
2019-08-17 13:15:25 -0400

    Sorted data, added features to analyze arrival rate

--- preprocessing-ems-data.py
+++ preprocessing-ems-data.py
@@ -3,7 +3,20 @@ from PythonWork import export_to_csv
 
 from _datetime import datetime
 
-# All of the following commented out code is to show all the preprocessing steps I went through
+
+def isLeap(year):
+    if (year % 4) == 0:
+        if (year % 100) == 0:
+            if (year % 400) == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+    # All of the following commented out code is to show all the preprocessing steps I went through
 # data = pd.read_csv('ems-incident-dispatch-data.csv')
 # # data is now a pandas dataframe
 #
@@ -21,17 +34,53 @@ from _datetime import datetime
 
 # data.FIRST_HOSP_ARRIVAL_DATETIME = pd.to_datetime(data.FIRST_HOSP_ARRIVAL_DATETIME, format='%Y-%m-%dT%H:%M:%S')
 
-data = pd.read_csv('ems-data-4.0.csv')
-data["FIRST_HOSP_ARRIVAL_DATETIME"] = pd.to_datetime(data.FIRST_HOSP_ARRIVAL_DATETIME)
-# print(data)
-# a = data.at[1, 'FIRST_HOSP_ARRIVAL_DATETIME']
+# data["FIRST_HOSP_ARRIVAL_DATETIME"] = pd.to_datetime(data.FIRST_HOSP_ARRIVAL_DATETIME)
 
 
 def returnday(date):
     return date.weekday()
 
 
-data['weekday'] = data['FIRST_HOSP_ARRIVAL_DATETIME'].apply(lambda x: returnday(x))
+def return_percent_of_year(date):
+    if isLeap(date.year):
+        return date.timetuple().tm_yday/366
+    else:
+        return date.timetuple().tm_yday/365
 
-export_to_csv.export(data, "final-ems-data.csv")
 
+# data['weekday'] = data['FIRST_HOSP_ARRIVAL_DATETIME'].apply(lambda x: returnday(x))
+
+# data['percent_of_year'] = data['FIRST_HOSP_ARRIVAL_DATETIME'].apply(lambda x: return_percent_of_year(x))
+#
+# data = data.sort_values(by=['FIRST_HOSP_ARRIVAL_DATETIME'])
+
+
+count = 0
+less_than_thirty = True
+
+
+def arrival_rate(date):
+    time = date.time().minute
+    global less_than_thirty
+    global count
+    if time < 30 and less_than_thirty is True:
+        count += 1
+        return count
+    elif time >= 30 and less_than_thirty is True:
+        less_than_thirty = False
+        count = 1
+        return count
+    elif time >= 30 and less_than_thirty is False:
+        count += 1
+        return count
+    elif time < 30 and less_than_thirty is False:
+        less_than_thirty = True
+        count = 1
+        return count
+
+
+data = pd.read_csv('ems-data.csv')
+
+data['how_many_come_in'] = data["FIRST_HOSP_ARRIVAL_DATETIME"].apply(lambda x: arrival_rate(x))
+
+export_to_csv.export(data, "final_process")
